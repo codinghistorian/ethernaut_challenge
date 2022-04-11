09bad4a0b10f592f5476e30de8e43b3b006664ae
codinghistorian <gaudihaus@gmail.com>
2022-04-11 17:14:39 +0900

    220411 17:14 KST solved 14_GateKeeperTwo.sol haha

--- 14_CalculateKeccak.sol
+++ 14_CalculateKeccak.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 contract CalculateKeccak {
     uint64 public leftHand;
     function calculateKeccak(address _origin) external {
-    leftHand = uint64(bytes8(keccak256(abi.encodePacked(msg.sender))));
+    leftHand = uint64(bytes8(keccak256(abi.encodePacked(_origin))));
     }
 }
 
--- 14_PWDGateKeeper2.sol
+++ 14_PWDGateKeeper2.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract Bullet {
-    constructor(bytes8 _gateKey) {
-        address target = 0xd9145CCE52D386f254917e481eB44e9943F39138;
-        target.call(
-            abi.encodeWithSignature("enter(bytes8)", _gateKey)
+    constructor(address __target, bytes8 __gateKey) {
+        // address target = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+        __target.call(
+            abi.encodeWithSignature("enter(bytes8)", __gateKey)
         );
     }
 }
 
 contract AK47 {
-    function shoot(bytes8 _gateKey) external {
-        Bullet bullet = new Bullet(_gateKey);
+    function shoot(address _target, bytes8 _gateKey) external {
+        Bullet bullet = new Bullet(_target, _gateKey);
     }
 }
 
